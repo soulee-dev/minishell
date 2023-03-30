41948426cf95939360489808f5b5eb5ea4c7da2d
soulee <me@soulee.dev>
2023-03-30 18:53:17 +0900

    fix: parse_quotes when line contain space

--- minishell.c
+++ minishell.c
@@ -60,7 +60,7 @@ int	main(int argc, char *argv[], char **envp)
 			cmd_list = parse_line(&cmd_list, line);
 			parse_dollar_sign(cmd_list, env_list);
 			parse_quotes(cmd_list);
-			iter_node(cmd_list);
+			// iter_node(cmd_list);
 			g_exit_code = execute_main(cmd_list, env_list,
 					count_cmd_list_node(cmd_list, TYPE_PIPE) + 1);
 			clear_cmd_list(&cmd_list);
--- parser.c
+++ parser.c
@@ -72,7 +72,8 @@ void	parse_quotes(t_cmd_list *cmd_list)
 			{
 				if (!quotes)
 				{
-					add_cmd_node_back(&arg_list, create_new_cmd_node(0, ft_strdup(str), 0));
+					if (str)
+						add_cmd_node_back(&arg_list, create_new_cmd_node(0, ft_strdup(str), 0));
 					str = ft_free_str(str);
 				}
 			}
@@ -97,10 +98,8 @@ void	parse_quotes(t_cmd_list *cmd_list)
 			}
 		}
 		if (str)
-		{
 			add_cmd_node_back(&arg_list, create_new_cmd_node(0, ft_strdup(str), 0));
-			str = ft_free_str(str);
-		}
+		str = ft_free_str(str);
 		cmd_list->args = convert_args_lst(arg_list);
 		cmd_list = cmd_list->next;
 	}
