a7f2561dae6bec8f2f92699ef56f183b88fc4851
chobinee <tnqls991213@icloud.com>
2023-04-03 22:43:46 +0900

    fix: cd

--- builtins1.c
+++ builtins1.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtins1.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: soulee <soulee@student.42seoul.kr>         +#+  +:+       +#+        */
+/*   By: subcho <subcho@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/04 21:58:42 by soulee            #+#    #+#             */
-/*   Updated: 2023/03/25 01:35:44 by soulee           ###   ########.fr       */
+/*   Updated: 2023/04/03 22:43:46 by subcho           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,9 +38,20 @@ void	command_echo(const char **s, int no_newline)
 		ft_putchar_fd('\n', 1);
 }
 
-void	command_cd(const char *path)
+void	command_cd(t_env_list *env_list, const char *path)
 {
+	char	*old_path;
+	char	*new_path;
+
+	old_path = getcwd(NULL, 0);
+	old_path = ft_strjoin_no_free("OLDPWD=", old_path);
+	command_export(env_list, (const char **)ft_split(old_path, ' '));
+	ft_printf("old : %s\n", (const char **)ft_split(old_path, ' ')[0]);
 	chdir(path);
+	new_path = getcwd(NULL, 0);
+	new_path = ft_strjoin_no_free("PWD=", new_path);
+	ft_printf("new : %s\n", (const char **)ft_split(new_path, ' ')[0]);
+	command_export(env_list, (const char **)ft_split(new_path, ' '));
 }
 
 void	command_pwd(void)
